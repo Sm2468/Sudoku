48b1a069c4a88e70814ea3f3d2d3c8f6c8d57556
Britt0508 <britt2609@hotmail.com>
2019-09-10 16:22:55 +0200

    16x16 added and tried to apply some rules

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 numbers_filled_in = []
 numbers_not_possible = []
 statements = []
-row=""
-column=""
-getal=""
+row = ""
+column = ""
+getal = ""
+# Size moet uit het grote document met meerdere oplossingen gehaald worden.
+# de sudokus uit dit document gaan we veranderen naar de vorm van sudoku-example,
+# zodat we onderstaande code daarvoor kunnen gebruiken.
+size = 9
 
 
 def main():
@@ -22,20 +26,36 @@ def main():
     #print(file_contents)
     print(numbers_filled_in)
     for number in numbers_filled_in:
+
+        row = number[0]
+        column = number[1]
+        getal = number[2]
+
+        # if a number is filled in, set all other numbers on that position to negative
         if number != "":
             statements.append(number)
             #print(number+ " number ")
-            row = number[0]
-            column = number[1]
-            getal = number[2]
             #print(getal)
-            for i in range(1, 10):
+            for i in range(1, size+1):
                 new_number = row + column
                 if str(i) != getal:
                     new_number = new_number+str(i)
                     #print(new_number + " new")
                     numbers_not_possible.append(new_number)
                     statements.append("-" + new_number)
+        # if a number is filled in, set every position in the same column, row and box negative for that number.
+        else:
+            for i in range(1, size+1):
+                if i != row:
+                    new_number = i+column+getal
+                    numbers_not_possible.append(new_number)
+                    statements.append("-" + new_number)
+                if j != column:
+                    new_number = row+j+getal
+                    numbers_not_possible.append(new_number)
+                    statements.append("-" + new_number)
+
+
 
     print(numbers_not_possible)
     print(statements)
